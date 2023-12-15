1e44b65f5d6d773fb31cb7f6375f74d2c74af751
nurjaman1996 <nurjamanmuhammad@gmail.com>
2023-12-16 06:57:52 +0700

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
 import Image from 'next/image'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      asdasd
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <div className='w-3/4'>
+        <Card>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
+      </div>
+
     </main>
   )
 }
